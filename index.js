9bc084511ac15da752efaa233dd419cb6ed2b2bc
Akash Bhaskar <i.am.akashbhaskar@accubits.com>
2021-11-05 22:26:50 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express()
+require('dotenv').config()
+const multer = require('multer')
+const path = require('path')
+
+const imageStorage = multer.diskStorage({
+    destination: 'images',
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '_' + Date.now()
+            + path.extname(file.originalname))
+
+    }
+})
+const imageUpload = multer({
+    storage: imageStorage,
+    limits: {
+        fileSize: 1000000 // 1000000 Bytes = 1 MB
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(png|jpg)$/)) {
+            // upload only png and jpg format
+            return cb(new Error('Please upload a Image'))
+        }
+        cb(undefined, true)
+    }
+})
+app.post('/uploadImage', imageUpload.single('image'), (req, res) => {
+    res.send(req.file)
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
+})
+
+app.listen(process.env.PORT, (err) => {
+    if (err) {
+        console.log('Error ', err);
+    }
+    console.log('Node.js is running at PORT', process.env.PORT)
+})
+
+
+
